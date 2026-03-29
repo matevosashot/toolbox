adf359f6f6f27dcd36c83a6c38bf23746dd5cbc6
matevosashot <ashmat98@gmail.com>
2026-03-29 17:39:39 +0400

    expose setup_loggers, report_errors to the top level

--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,3 +1,7 @@
 from .machine import get_hostname, get_launch_info, git_info, get_local_ip
+from .logging_utils import setup_loggers, get_telegram_handler, get_file_handler, report_errors
 
-__all__ = ["get_hostname", "get_launch_info", "git_info", "get_local_ip"]
+__all__ = [
+    "get_hostname", "get_launch_info", "git_info", "get_local_ip",
+    "setup_loggers", "get_telegram_handler", "get_file_handler", "report_errors",
+]
