b774ac77b0b7e9f4ab1dc5335529943faaa8d813
matevosashot <ashmat98@gmail.com>
2026-04-17 22:25:33 +0400

    add path to the package

--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,5 +1,6 @@
 from .machine import get_hostname, get_launch_info, git_info, get_local_ip
 from .logging_utils import setup_loggers, get_telegram_handler, get_file_handler, report_errors
+from . import path
 
 __all__ = [
     "get_hostname", "get_launch_info", "git_info", "get_local_ip",
